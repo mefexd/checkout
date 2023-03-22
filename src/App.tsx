c8b3ce1f843734aeff515c9265760944dd7d6077
codecreep <codecreep@proton.me>
2023-03-22 22:03:43 +0000

    remove unused prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { Basket } from "./components/Basket";
-import { ProductListMockData } from "./shared/mocks/mockProductList";
 
 function App() {
   return (
     <div className="App">
       <h1>Checkout</h1>
-      <Basket BasketItems={ProductListMockData} />
+      <Basket />
     </div>
   );
 }
